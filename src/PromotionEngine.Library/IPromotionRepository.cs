4bb8789a1f1912493cb5878b858a8f9375f3e61c
Zsolt Keczer <keczerzsolt@gmail.com>
2021-01-28 20:51:37 +0100

    InMemoryPromotionRepository class

--- src/PromotionEngine.Library/IPromotionRepository.cs
+++ src/PromotionEngine.Library/IPromotionRepository.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using PromotionEngine.Library.Model;
 
 namespace PromotionEngine.Library
 {
---
+++ src/PromotionEngine.Library/InMemoryPromotionRepository.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using PromotionEngine.Library.Model;
+using System.Linq;
+
+namespace PromotionEngine.Library
+{
+    public class InMemoryPromotionRepository : IPromotionRepository
+    {
+        private readonly IList<Promotion> promotions;
+
+        public InMemoryPromotionRepository(IList<Promotion> promotions)
+        {
+            this.promotions = promotions ?? throw new ArgumentNullException(nameof(promotions));
+        }
+
+        public IList<Promotion> GetPromotions(IList<Product> products)
+        {
+            if (products is null)
+            {
+                throw new ArgumentNullException(nameof(products));
+            }
+
+            return promotions
+                .Where(x => products.Any(y => x.DiscountableProducts.ContainsKey(y)))
+                .ToList();
+        }
+    }
+}
