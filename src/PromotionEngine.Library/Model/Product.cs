975d9db5869adeabde2e81477d6209681e7cd58c
Zsolt Keczer <keczerzsolt@gmail.com>
2021-01-28 20:00:16 +0100

    product class

--- src/PromotionEngine.Library/Model/Product.cs
+++ src/PromotionEngine.Library/Model/Product.cs
@@ -4,8 +4,24 @@ namespace PromotionEngine.Library.Model
 {
     public class Product
     {
-        //Sku
+        public string Sku { get; }
 
-        //price
+        public double Price { get; }
+
+        public Product(string sku, double price)
+        {
+            Sku = sku ?? throw new ArgumentNullException(nameof(sku));
+            Price = price;
+        }
+
+        public override bool Equals(object obj) => 
+            obj is Product && this == (Product)obj;
+
+        public override int GetHashCode() => (Sku, Price).GetHashCode();
+        
+        public static bool operator ==(Product x, Product y) =>
+            x.Sku == y.Sku && x.Price == y.Price;
+        
+        public static bool operator !=(Product x, Product y) => !(x = y);
     }
 }
